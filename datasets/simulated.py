3c7fe152c43877a5412d1965991029af876f8b32
Cassio F. Dantas <cassiofragadantas@gmail.com>
2022-12-09 17:31:55 +0100

    Generating positive data locally instead of introducing a parameter "positive" in make_correlated_data

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -4,6 +4,7 @@ from benchopt.datasets.simulated import make_correlated_data
 
 with safe_import_context() as import_ctx:
     import numpy as np
+    from numpy.linalg import norm
 
 
 class Dataset(BaseDataset):
@@ -14,25 +15,36 @@ class Dataset(BaseDataset):
     # the cross product for each key in the dictionary.
     parameters = {
         'n_samples, n_features': [(100, 5000), (100, 10000)],
-        'pos_data': [True, False],
+        'positive': [True, False],
     }
 
-    def __init__(self, n_samples=10, n_features=50, pos_data=False, rho=0,
-                 random_state=27):
+    def __init__(self, n_samples=10, n_features=50, positive=False, rho=0,
+                 snr=3, random_state=27):
         # Store the parameters of the dataset
         self.n_samples = n_samples
         self.n_features = n_features
-        self.pos_data = pos_data
+        self.positive = positive
         self.random_state = random_state
         self.rho = rho
+        self.snr = snr
 
     def get_data(self):
 
         rng = np.random.RandomState(self.random_state)
 
-        X, y, _ = make_correlated_data(self.n_samples, self.n_features,
-                                       rho=self.rho, random_state=rng,
-                                       pos_data=self.pos_data)
+        X, y, w_true = make_correlated_data(self.n_samples, self.n_features,
+                                            rho=self.rho, random_state=rng)
+
+        if self.positive:
+            X = np.abs(X)
+            w_true = np.abs(w_true)
+            # Regenerate y
+            y = X @ w_true
+            noise = rng.randn(self.n_samples)
+            if self.snr not in [0, np.inf]:
+                y += noise / norm(noise) * norm(y) / self.snr
+            elif self.snr == 0:
+                y = noise
 
         data = dict(X=X, y=y)
 
